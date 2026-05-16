59334e8896d0e1a59e002d253fdbf3e9bc4ba008
Thang Le <thangle9804@gmail.com>
2026-05-16 22:00:38 +0700

    Add day 16, 17, 18 - project 1

---
+++ Projects/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,72 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Thang Le on 14/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount: Double = 0
+    @State private var numberOfPeopleIndex = 0
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+
+    private var totalAmount: Double {
+        return checkAmount * (1 + Double(tipPercentage) / 100)
+    }
+
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeopleIndex) + 2 // convert index to people count
+
+        return totalAmount / Double(peopleCount)
+    }
+
+    let amountFormat: FloatingPointFormatStyle<Double>.Currency = .currency(code: Locale.current.currency?.identifier ?? "USD")
+
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: amountFormat).keyboardType(.numberPad)
+                        .focused($amountIsFocused)
+
+                    Picker("Number of people", selection: $numberOfPeopleIndex) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
+                        }
+                    }.pickerStyle(.navigationLink)
+                }
+
+                Section("How much tip to leave?") {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ... 100, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }.pickerStyle(.navigationLink)
+                }
+
+                Section("Total amount") {
+                    Text(totalAmount, format: amountFormat)
+                }
+
+                Section("Amount per person") {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Projects/WeSplit/WeSplitTests/WeSplitTests.swift
@@ -0,0 +1,19 @@
+//
+//  WeSplitTests.swift
+//  WeSplitTests
+//
+//  Created by Thang Le on 14/5/26.
+//
+
+import Testing
+@testable import WeSplit
+
+struct WeSplitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
