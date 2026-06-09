dec0ac7d2bca9fbae6aecfd7b038e9a500665b85
Thang Le <thangle9804@gmail.com>
2026-06-09 08:44:57 +0700

    (In progress) Add BetterRest - Project 4

---
+++ Projects/BetterRest-Project4-Day26-28/BetterRest-Project4-Day26-28/BetterRest_Project4_Day26_28App.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRest_Project4_Day26_28App.swift
+//  BetterRest-Project4-Day26-28
+//
+//  Created by Thang Le on 6/6/26.
+//
+
+import SwiftUI
+
+@main
+struct BetterRest_Project4_Day26_28App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
