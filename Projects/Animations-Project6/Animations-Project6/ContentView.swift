dbed32f5f4f06a4184788d7709264c0d23f8325a
Thang Le <thangle9804@gmail.com>
2026-06-14 00:27:45 +0700

    Animations-Project 6: Add more advance gesture animation with delay

--- Projects/Animations-Project6/Animations-Project6/ContentView.swift
+++ Projects/Animations-Project6/Animations-Project6/ContentView.swift
@@ -8,23 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var dragAmount = CGSize.zero
+    let letters = Array("Hello SwiftUI")
+        @State private var enabled = false
+        @State private var dragAmount = CGSize.zero
 
-    var body: some View {
-        LinearGradient(colors: [.yellow, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
-            .frame(width: 300, height: 200)
-            .clipShape(.rect(cornerRadius: 10))
-            .offset(dragAmount)
+        var body: some View {
+            HStack(spacing: 0) {
+                ForEach(0..<letters.count, id: \.self) { num in
+                    Text(String(letters[num]))
+                        .padding(5)
+                        .font(.title)
+                        .background(enabled ? .blue : .red)
+                        .offset(dragAmount)
+                        .animation(.linear.delay(Double(num) / 20), value: dragAmount)
+                }
+            }
             .gesture(
                 DragGesture()
-                    .onChanged { arg in dragAmount = arg.translation }
+                    .onChanged { dragAmount = $0.translation }
                     .onEnded { _ in
-                        withAnimation(.bouncy(extraBounce: 0.3)) {
-                            dragAmount = .zero
-                        }
+                        dragAmount = .zero
+                        enabled.toggle()
                     }
             )
-    }
+        }
 }
 
 #Preview {
