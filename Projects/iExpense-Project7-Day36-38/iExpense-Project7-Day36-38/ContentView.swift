b35491883e861ddcc4a35f6227d914d4b555b023
Thang Le <thangle9804@gmail.com>
2026-07-29 14:20:31 +0700

    Project 7 - day 36 - 38: try showing view as sheet; dismiss sheet with DismissAction from @Environment

--- Projects/iExpense-Project7-Day36-38/iExpense-Project7-Day36-38/ContentView.swift
+++ Projects/iExpense-Project7-Day36-38/iExpense-Project7-Day36-38/ContentView.swift
@@ -8,21 +8,24 @@
 import SwiftUI
 import Observation
 
-@Observable
-class User {
-    var firstName = "Bilbo"
-    var lastName = "Baggins"
+struct ContentView: View {
+    @State private var showingSheet = false
+    
+    var body: some View {
+            Button("Show Sheet") {
+                showingSheet.toggle()
+            }.sheet(isPresented: $showingSheet) {
+                SecondView()
+            }
+        }
 }
 
-struct ContentView: View {
-    @State private var user = User()
-
+struct SecondView: View {
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        VStack {
-            Text("Your name is \(user.firstName) \(user.lastName).")
-
-            TextField("First name", text: $user.firstName)
-            TextField("Last name", text: $user.lastName)
+        Button("Dismiss") {
+            dismiss()
         }
     }
 }
